e9e0fa2e3d05c33e5f9581b13ea3f16ba4dc5509
Hamit <hamitsehjal26@gmail.com>
2022-09-12 18:37:30 -0400

    added some data to demonstrate tab characters

--- w1p2.c
+++ w1p2.c
@@ -12,5 +12,15 @@ int main()
     printf("\\ this is a back-slash character\n");
     printf("%% this is a back-slash character\n"); // How to print %(percentage character) using printf
     printf("\" this is a back-slash character\n");
+
+    printf("My favorite quotes are:\n");
+    printf("1.\t\"Skill is only developed by hours and hours of work.\n");
+    printf("\t\t\t\t\t\t-Usain Bolt\n");
+    printf("2.\t\"It's not about having time. It's about making time.\n");
+    printf("\t\t\t\t\t\t-unknown\n");
+    printf("3.\t\"All of us do not have equal talent. But, all of us\n");
+    printf("\thave an equal opportunity to develop our talents.\n");
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+
     return 0;
 }
