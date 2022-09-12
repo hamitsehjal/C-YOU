f5c27976022e97732f8e42e8c5ac97e651ca9295
Hamit <hamitsehjal26@gmail.com>
2022-09-12 18:40:14 -0400

    formatted my data

--- w1p2.c
+++ w1p2.c
@@ -6,21 +6,21 @@ int main()
     printf("=================\n");
 
     printf("Using tab specifiers...\n");
-    printf("\tTab-1\tTab-2\tTab-2\n");
-    printf("\t----\t====\t----\n");
+    printf("\t\tTab-1\tTab-2\tTab-3\n");
+    printf("\t\t----\t====\t----\n");
 
     printf("\\ this is a back-slash character\n");
     printf("%% this is a back-slash character\n"); // How to print %(percentage character) using printf
-    printf("\" this is a back-slash character\n");
+    printf("\" this is a back-slash character\n\n");
 
     printf("My favorite quotes are:\n");
     printf("1.\t\"Skill is only developed by hours and hours of work.\n");
-    printf("\t\t\t\t\t\t-Usain Bolt\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t-Usain Bolt\n");
     printf("2.\t\"It's not about having time. It's about making time.\n");
-    printf("\t\t\t\t\t\t-unknown\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t-unknown\n");
     printf("3.\t\"All of us do not have equal talent. But, all of us\n");
-    printf("\thave an equal opportunity to develop our talents.\n");
-    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+    printf(" \thave an equal opportunity to develop our talents.\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
 
     return 0;
 }
