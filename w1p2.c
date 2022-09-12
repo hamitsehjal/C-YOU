44dade7b906b59427983d6334abf1b7052134b67
Hamit <hamitsehjal26@gmail.com>
2022-09-12 18:29:37 -0400

    Learnt  how to print %percentage character

--- w1p2.c
+++ w1p2.c
@@ -3,6 +3,14 @@
 int main()
 {
     printf("Workshop 1 Part-2\n");
-    printf("=================");
+    printf("=================\n");
+
+    printf("Using tab specifiers...\n");
+    printf("\tTab-1\tTab-2\tTab-2\n");
+    printf("\t----\t====\t----\n");
+
+    printf("\\ this is a back-slash character\n");
+    printf("%% this is a back-slash character\n"); // How to print %(percentage character) using printf
+    printf("\" this is a back-slash character\n");
     return 0;
 }
